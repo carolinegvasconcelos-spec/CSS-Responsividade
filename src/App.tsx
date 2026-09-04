8776c3e3300fba4e87cd46d61d257716a244fb51
Caroline Vasconcelos <carolinegvasconcelos@gmail.com>
2026-09-04 13:54:25 +0000

    css-responsivel

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App(){
+
+  return(
+    <>
+    
+    <nav>
+      Barra de navegação
+    </nav>
+    <button className='botao-flutuante'>
+     +
+    </button>
+    <p className>Tela detectada</p>
+    
+
+    </>
+  )
+}
+  
+export default App
